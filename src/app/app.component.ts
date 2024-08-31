92dbc88a0a7a7ab3342bac361eece873e9c6e759
Rawanqawareeq <Rawanqawareeq95@gmail.com>
2024-08-31 22:57:43 +0300

    angular code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,23 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component} from '@angular/core';
+import { HeaderComponent } from './header/header.component';
+import { UserComponent } from './user/user.component';
+import { DUMMY_USERS } from './user/dummy-user';
+import { TasksComponent } from './tasks/tasks.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [HeaderComponent,UserComponent,TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'project';
+  users = DUMMY_USERS;
+  SelectedUserId = 'u2';
+  get selectedUser(){
+    return this.users.find((user)=>{return user.id === this.SelectedUserId});
+  }
+  onselectorUser(id:string){
+    this.SelectedUserId = id;
+  }
 }
---
+++ src/app/user/user.component.ts
@@ -0,0 +1,35 @@
+import { Component, computed,  Input,EventEmitter, Output, input   } from '@angular/core';
+import { DUMMY_USERS } from './dummy-user';
+/* type User = {
+  id:string;
+  avatar:string;
+  name:string;
+ }*/
+interface User {
+  id:string;
+  avatar:string;
+  name:string;
+ }
+const randromIndex =  Math.floor(Math.random() *  DUMMY_USERS.length);
+@Component({
+  selector: 'app-user',
+  standalone: true,
+  imports: [],
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css'
+})
+export class UserComponent {
+ @Input({required: true}) user!:User;
+  @Output() select = new EventEmitter<string>();
+   //id = input.required<string>();
+ 
+  // select = output<string>();
+   //selectorUser = signal(DUMMY_USERS[randromIndex]) ;
+   //imagePath = computed(()=>'assets/users/' + this.selectorUser().avatar);
+   get imagePath(){
+     return 'assets/users/' + this.user.avatar;
+   }
+   onselectorUser(){
+    this.select.emit(this.user.id);
+   }
+}
